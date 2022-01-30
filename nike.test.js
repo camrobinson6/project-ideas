fc54387c877d52b61e90afc7f3b1785b8c56471b
Cameron Robinson <camrobinson6@gmail.com>
2022-01-30 13:25:40 -0600

    adding comments

--- nike.test.js
+++ nike.test.js
@@ -4,7 +4,7 @@ const chromedriver = require('chromedriver')
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
-jest.setTimeout(100000)
+jest.setTimeout(100000) //max timeout for test to run
 
 beforeAll(async () => {
     await driver.get("https://www.nike.com/")
@@ -17,56 +17,56 @@ afterAll(async() => {
 describe("NIKE app functionality", () => {
     //the modal is the first thing to test on the page to make sure testing is correct
         it("Must authenticate the Jordan page", async () => {
-            
+    //testing the jordan logo button        
             let jordanLogo = await driver.findElement(By.xpath('//*[@id="gen-nav-commerce-header-v2"]/div[3]/div[1]/div/ul/li[1]/a'))
-            await jordanLogo.click()
-            await driver.sleep(4000)
-
+            await jordanLogo.click() //this is where I click on the button when testing
+            await driver.sleep(4000) //this how long the test waits before performing task
+    //testing the jordan community 
             let jordanCom = await driver.findElement(By.xpath('//*[@id="debfdce8-2b07-4728-a361-405a9c3804a6"]/div/div/div/nav/div[1]/ul/li[3]/a'))
-            await jordanCom.click()
-            await driver.sleep(3000)
+            await jordanCom.click() //this is where I click on the button when testing
+            await driver.sleep(3000) //this how long the test waits before performing task
         })
 
         it("Test the New Release page", async () => {
-
+    //testing the new release page
             let newRelease = await driver.findElement(By.xpath('//*[@id="gen-nav-commerce-header-v2"]/div[3]/header/div/div[1]/div[2]/nav/div[2]/ul/li[1]/a'))
-            await newRelease.click()
-            await driver.sleep(3000)
+            await newRelease.click() //this is where I click on the button when testing
+            await driver.sleep(3000) //this how long the test waits before performing task
         })
 
         it("Test the Nike Men's page", async () => {
-
+    //testing the nike mens page
             let mensPage = driver.findElement(By.xpath('//*[@id="gen-nav-commerce-header-v2"]/div[3]/header/div/div[1]/div[2]/nav/div[2]/ul/li[2]/a'))
-            await mensPage.click()
-            await driver.sleep(2000)
+            await mensPage.click() //this is where I click on the button when testing
+            await driver.sleep(2000) //this how long the test waits before performing task
          })
 
         it("Test the Nike Women's page", async () => {
-
+    //testing the Womens page
             let womensPage = driver.findElement(By.xpath('//*[@id="gen-nav-commerce-header-v2"]/div[3]/header/div/div[1]/div[2]/nav/div[2]/ul/li[3]/a'))
-            await womensPage.click()
-            await driver.sleep(2000)
+            await womensPage.click() //this is where I click on the button when testing
+            await driver.sleep(2000) //this how long the test waits before performing task
 
         })
         it("Test the Nike Kid's page", async () => {
 
-            let kidsPage = driver.findElement(By.xpath('//*[@id="gen-nav-commerce-header-v2"]/div[3]/header/div/div[1]/div[2]/nav/div[2]/ul/li[4]/a'))
-            await kidsPage.click()
-            await driver.sleep(2000)
+            let kidsPage = driver.findElement(By.xpath('//*[@id="gen-nav-commerce-header-v2"]/div[3]/header/div/div[1]/div[2]/nav/div[2]/ul/li[4]/a')) //testing the kids page
+            await kidsPage.click() //this is where I click on the button when testing
+            await driver.sleep(2000) //this how long the test waits before performing task
         })
 
         it("Test the Search bar works", async () => {
-
+    //testing the search bar input
             let search = driver.findElement(By.xpath('//*[@id="VisualSearchInput"]'))
-            await search.click()
-            await search.sendKeys('Huaraches')
-            await driver.sleep(2000)
+            await search.click() //this is where I click on the button when testing
+            await search.sendKeys('Huaraches') //this testing is when you search you input "huaraches"
+            await driver.sleep(2000) //this how long the test waits before performing task
         })
 
         it("Test the About page", async () => {
-
+    //testing the about me button
             let aboutBtn = driver.findElement(By.xpath('//*[@id="gen-nav-footer"]/footer/div/div[1]/div[1]/div/div[3]/div/ul/li[2]/a'))
-            await aboutBtn.click()
-            await driver.sleep(4000)
+            await aboutBtn.click() //this is where I click on the button when testing
+            await driver.sleep(4000) //this how long the test waits before performing task
         })
     })
--- slam.test.js
+++ slam.test.js
@@ -4,7 +4,7 @@ const chromedriver = require('chromedriver')
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 //setTimeout to this input cause I ran out of time cause some features need time to load up
-jest.setTimeout(100000)
+jest.setTimeout(100000) //max time for testing to run before it close out
 
 beforeAll(async () => {
     await driver.get("https://www.slamonline.com/")
@@ -19,96 +19,96 @@ describe("SLAM app functionality", () => {
 //the modal is the first thing to test on the page to make sure testing is correct
     it("Must authenticate via Slam News page", async () => {
         let modalAgree = await driver.findElement(By.xpath('//*[@id="qc-cmp2-ui"]/div[2]/div/button[3]'))
-        await modalAgree.click()
+        await modalAgree.click()  //this clicks on the covers button
         await driver.sleep(4000)
     })
     it("The Menu dropdown works", async () => {
 //testing the menu button and closing it out
         let menu = await driver.findElement(By.xpath('//*[@id="menu-btn-inner"]/div'))
-        await menu.click()
+        await menu.click()  //this clicks on the covers button
         await driver.sleep(2000)
 
         let cross = await driver.findElement(By.xpath('//*[@id="menu-btn-inner"]/div'))
-        await cross.click()
+        await cross.click()  //this clicks on the covers button
         await driver.sleep(1000)
     })
     it("Authenticate NBA News", async () => {
 //testing the News section and loading more pages
         let secMenu = await driver.findElement(By.xpath('//*[@id="menu-btn-inner"]/div'))
-        await secMenu.click()
+        await secMenu.click()  //this clicks on the covers button
         await driver.sleep(2000)
 
         let news = await driver.findElement(By.xpath('//*[@id="menu-item-719221"]'))
-        await news.click()
+        await news.click()  //this clicks on the covers button
         await driver.sleep(2000)
 
         let nba = driver.findElement(By.xpath('//*[@id="menu-item-719222"]/a'))
-        await nba.click()
+        await nba.click()  //this clicks on the covers button
         await driver.sleep(1000)
 
         let loadMore = driver.findElement(By.xpath('//*[@id="ajax-load-more"]/div[2]/button'))
-        await loadMore.click()
+        await loadMore.click()  //this clicks on the covers button
         await driver.sleep(2000)
 
     })
     it("Must authenticate College baskeball page", async () => {
-//testing college basketball page        
+//testing the menu button    
         let thirdMenu = await driver.findElement(By.xpath('//*[@id="menu-btn-inner"]/div'))
-        await thirdMenu.click()
+        await thirdMenu.click()  //this clicks on the covers button
         await driver.sleep(2000)
-
+//testing the News button
         let newsTwo = await driver.findElement(By.xpath('//*[@id="menu-item-719221"]'))
-        await newsTwo.click()
+        await newsTwo.click()  //this clicks on the covers button
         await driver.sleep(2000)
-
+//testing the collegebasketball and highschool basketball button
         let cbk = await driver.findElement(By.xpath('//*[@id="menu-item-719224"]/a'))
-        await cbk.click()
+        await cbk.click() //this clicks on the covers button
         await driver.sleep(1000)
-
+//testing the load more button
         let loadMoretwo = driver.findElement(By.xpath('//*[@id="ajax-load-more"]/div[2]/button'))
-        await loadMoretwo.click()
+        await loadMoretwo.click()  //this clicks on the covers button
         await driver.sleep(2000)
 })
     it("Must authenticate WNBA page", async () => {
- //testing WNBA page       
+ //testing WNBA page and //testing the menu button
         let forthMenu = await driver.findElement(By.xpath('//*[@id="menu-btn-inner"]/div'))
-        await forthMenu.click()
+        await forthMenu.click()  //this clicks on the covers button
         await driver.sleep(2000)
-
+//testing the News button
         let newsTri = await driver.findElement(By.xpath('//*[@id="menu-item-719221"]'))
-        await newsTri.click()
+        await newsTri.click()  //this clicks on the covers button
         await driver.sleep(2000)
-
+//testing the WNBA button
         let wnba = await driver.findElement(By.xpath('//*[@id="menu-item-719223"]/a'))
-        await wnba.click()
+        await wnba.click()  //this clicks on the covers button
         await driver.sleep(1000)
-
+//testing the load more button
         let loadMoretri = driver.findElement(By.xpath('//*[@id="ajax-load-more"]/div[2]/button'))
-        await loadMoretri.click()
+        await loadMoretri.click() //this clicks on the covers button
         await driver.sleep(2000)
 })
     it("Must authenticate the Features page", async () => {
-//testing feature page works
+//testing feature page works and the menu button
         let fivMenu = await driver.findElement(By.xpath('//*[@id="menu-btn-inner"]/div'))
-        await fivMenu.click()
+        await fivMenu.click() //this clicks on the covers button
         await driver.sleep(2000)
-
+//testing the feature button on site
         let feature = await driver.findElement(By.xpath('//*[@id="menu-item-721479"]/a'))
-        await feature.click()
+        await feature.click()  //this clicks on the covers button
         await driver.sleep(2000)
-
+//testing the load more button
         let featureMore = await driver.findElement(By.xpath('//*[@id="ajax-load-more"]/div[2]/button'))
-        await featureMore.click()
+        await featureMore.click()  //this clicks on the covers button
         await driver.sleep(2000)
 })
     it("Must authenticate the Covers page", async () => {
 //testing the Covers page
         let sixMenu = await driver.findElement(By.xpath('//*[@id="menu-btn-inner"]/div'))
-        await sixMenu.click()
+        await sixMenu.click()  //this clicks on the covers button
         await driver.sleep(3000)
-
+//testing the covers button
         let covers = await driver.findElement(By.xpath('//*[@id="menu-item-719236"]/a'))
-        await covers.click()
+        await covers.click() //this clicks on the covers button
         await driver.sleep(18000)
 
       
